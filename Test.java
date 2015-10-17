9c9b4619731eb7dd3ab1a1af544bf342239e5baa
Scieon <anhkhoi.vunguyen@gmail.com>
2015-10-17 14:27:14 -0400

    Added power function

--- Test.java
+++ Test.java
@@ -12,6 +12,8 @@ public class Test {
 		endTime = System.nanoTime(); 
 		duration = endTime - startTime;
 		System.out.println("Nano seconds: " + duration);
+		
+		System.out.println(pow(5,10));
 	}
 	
 	
@@ -27,12 +29,26 @@ public class Test {
 	
 	//Possible stack overflow, but shorter runtime.
 	public static int recFact(int n){
-		
 		if(n<1)
 			return 1;
 		return n*recFact(n-1);
+	}
+	
+	public static int pow(int x, int n){
+		int y;
+		if(n==0)
+			return 1;
 		
+		else if(n%2==1){
+			y = pow(x,(n-1)/2);
+			return x*y*y;
+		}
 		
+		else{
+			y = pow(x,n/2);
+			return y*y;
+		}
+			
 	}
 }
 
